d2025e475fc06a559e1466b4cf1a85acd8ba08fb
Ray Sanders <raymond.sanders@gmail.com>
2019-05-14 23:06:15 -0500

    added performanceReviews endpoint to return review phrases

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const employeeJson = require('./data/employeeData');
+const performanceReviewPhrasesJson = require('./data/performanceReviewPhrases');
 const app = express();
 
-app.get('/employees', (req, res) => res.send(employeeJson));
+app.get('/employees', (req, res) => res.status(200).send(employeeJson));
+app.get('/performanceReviews', (req, res) => res.status(200).send(performanceReviewPhrasesJson));
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('./app');
 const employeeJson = require('./data/employeeData');
+const performanceReviewPhrasesJson = require('./data/performanceReviewPhrases');
 
 describe('mockHRSystem', () => {
     describe('employees GET', () => {
@@ -14,5 +15,17 @@ describe('mockHRSystem', () => {
                 expect(response.status).toBe(200);
             })
         });
-    })
+    });
+    describe('performance reviews GET', () => {
+        test('returns performance reviews JSON', () => {
+            return request(app).get('/performanceReviews').then(response => {                
+                expect(response.body).toStrictEqual(performanceReviewPhrasesJson);
+            })
+        });
+        test('returns status code 200', () => {
+            return request(app).get('/performanceReviews').then(response => {                
+                expect(response.status).toBe(200);
+            })
+        });
+    });
 })
