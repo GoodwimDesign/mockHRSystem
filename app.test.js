c7df29e229c008a1bff0752ab8dfbf98cd2485c9
Ray Sanders <raymond.sanders@gmail.com>
2019-05-15 07:39:44 -0500

    Reformat tests

--- app.test.js
+++ app.test.js
@@ -6,26 +6,34 @@ const performanceReviewPhrasesJson = require('./data/performanceReviewPhrases');
 describe('mockHRSystem', () => {
     describe('employees GET', () => {
         test('returns employee JSON', () => {
-            return request(app).get('/employees').then(response => {                
+            return request(app)
+            .get('/employees')
+            .then(response => {
                 expect(response.body).toStrictEqual(employeeJson);
-            })
+            });
         });
         test('returns status code 200', () => {
-            return request(app).get('/employees').then(response => {                
+            return request(app)
+            .get('/employees')
+            .then(response => {
                 expect(response.status).toBe(200);
-            })
+            });
         });
     });
     describe('performance reviews GET', () => {
         test('returns performance reviews JSON', () => {
-            return request(app).get('/performanceReviews').then(response => {                
+            return request(app)
+            .get('/performanceReviews')
+            .then(response => {
                 expect(response.body).toStrictEqual(performanceReviewPhrasesJson);
-            })
+            });
         });
         test('returns status code 200', () => {
-            return request(app).get('/performanceReviews').then(response => {                
+            return request(app)
+            .get('/performanceReviews')
+            .then(response => {
                 expect(response.status).toBe(200);
-            })
+            });
         });
     });
-})
+});
