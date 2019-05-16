3347421934f7c084b9c1bfa4ddea9f3acbd5278c
Ray Sanders <raymond.sanders@gmail.com>
2019-05-15 22:55:59 -0500

    WIP - new routes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const performanceReviewPhrasesJson = require('./data/performanceReviewPhrases');
 const app = express();
 
 app.get('/employees', (req, res) => res.status(200).send(employeeJson));
+
+//TODO: change this route to performanceReviewPhrases
 app.get('/performanceReviews', (req, res) => res.status(200).send(performanceReviewPhrasesJson));
+
+//TODO: add /performanceReviews route for returning combined employeeId and performance review data
 
 module.exports = app;
