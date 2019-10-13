8d7d1e0fe2cef1d58f5e14153b65d81750ecec4e
Ray Sanders <raymond.sanders@gmail.com>
2019-10-12 23:24:24 -0500

    Move buildReviewTest function out to avoid scoping issues

--- data/dataProcessor.js
+++ data/dataProcessor.js
@@ -1,26 +1,31 @@
-const getCombinedData = function (employeeJson, phrasesJson, numberOfEmployees = 1, sentiment = 'neutral') {
+const buildReviewText = function(index, reviewText, sentiment, phrasesJson) {
+    if (sentiment === 'positive') {
+        return reviewText.concat(phrasesJson.positive[index]);
+    } else {
+        return reviewText.concat(
+            phrasesJson.positive[index],
+            ' ',
+            phrasesJson.negative[index]
+        )
+    }
+};
+
+const getCombinedData = function (
+    employeeJson, 
+    phrasesJson, 
+    numberOfEmployees = 1, 
+    sentiment = 'neutral'
+    ) {
     const combinedData = [];
     let reviewText = '';
 
-    const buildReviewText = function(index) {
-        if (sentiment === 'positive') {
-            return reviewText.concat(phrasesJson.positive[index]);
-        } else {
-            return reviewText.concat(
-                phrasesJson.positive[index],
-                ' ',
-                phrasesJson.negative[index]
-            )
-        }
-    };
-    
-    for (let x = 0; x < numberOfEmployees; x++) {                
+    for (let x = 0; x < numberOfEmployees; x++) { 
         combinedData.push({
                 employeeId: employeeJson[x].employeeId,
                 performanceReviews: [{
                     reviewId: 1,
                     reviewDate: new Date('August 19, 1975').toString(),
-                    reviewText: buildReviewText(x),          
+                    reviewText: buildReviewText(x, reviewText, sentiment, phrasesJson),          
                 }]
             }
         );
